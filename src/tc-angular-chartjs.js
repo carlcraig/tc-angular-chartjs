4118f4ddd98622490375db1d8cac72e23606854b
danada <dan.desousa@gmail.com>
2014-09-29 17:24:12 -0400

    Explicitly destroy chart on data change

--- src/tc-angular-chartjs.js
+++ src/tc-angular-chartjs.js
@@ -97,8 +97,14 @@
 
             if ( value ) {
               if ( chartType ) {
+                if ( chartObj ) {
+                  chartObj.destroy();
+                }
                 chartObj = chart[ cleanChartName( chartType ) ]( $scope.data, $scope.options );
               } else if ($scope.type) {
+                if ( chartObj ) {
+                  chartObj.destroy();
+                }
                 chartObj = chart[ cleanChartName( $scope.type ) ]( $scope.data, $scope.options );
               } else {
                 throw 'Error creating chart: Chart type required.';
