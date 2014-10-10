725825361c71ef6e0339268b42a2a0995248dc78
Carl Craig <carlcraig@3c-studios.com>
2014-10-10 22:58:37 +0100

    Fixed main file in bower.json and update to version 1.0.6

--- dist/tc-angular-chartjs.min.js
+++ dist/tc-angular-chartjs.min.js
@@ -1,5 +1,5 @@
 /**
- * tc-angular-chartjs - v1.0.5 - 2014-09-29
+ * tc-angular-chartjs - v1.0.6 - 2014-10-10
  * Copyright (c) 2014 Carl Craig <carlcraig@3c-studios.com>
  * Dual licensed with the Apache-2.0 or MIT license.
  */
