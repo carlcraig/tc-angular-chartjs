0e8a63682737ed50c32ec13db4e8ac39e54592b0
Carl Craig <carlcraig@3c-studios.com>
2015-05-17 21:39:39 +0100

    Update version to v1.0.10

--- dist/tc-angular-chartjs.js
+++ dist/tc-angular-chartjs.js
@@ -1,5 +1,5 @@
 /**
- * tc-angular-chartjs - v1.0.9 - 2015-05-17
+ * tc-angular-chartjs - v1.0.10 - 2015-05-17
  * Copyright (c) 2015 Carl Craig <carlcraig@3c-studios.com>
  * Dual licensed with the Apache-2.0 or MIT license.
  */
@@ -65,6 +65,11 @@
                         autoLegend = true;
                     }
                 }
+                $scope.$on("$destroy", function() {
+                    if (chartObj) {
+                        chartObj.destroy();
+                    }
+                });
                 $scope.$watch("data", function(value) {
                     if (value) {
                         if (chartObj) {
